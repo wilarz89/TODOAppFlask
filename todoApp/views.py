89666bf2809ea2c4e7d372e1590b9de7675f861d
Wilson Ariza <wjarizatruquetkd@gmail.com>
2018-04-02 15:32:07 -0500

    Created login Template
    Template and routing for login created

--- todoApp/views.py
+++ todoApp/views.py
@@ -3,9 +3,15 @@ from flask import render_template
 from todoApp import app
 
 @app.route('/')
+
 def index():
     return render_template("index.html")
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/todoApp')
+def todoApp():
+    user = {'username':'Wilson'}
+    return  render_template('login.html',title='Login',user=user)
