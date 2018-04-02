423e765160321d46ac3782d8ac4512298980d776
Wilson Ariza <wjarizatruquetkd@gmail.com>
2018-04-02 15:08:52 -0500

    Corrected Model
    Decided to use simple mongoengine, Deleted flask modifications

--- models.py
+++ models.py
@@ -1,38 +1,51 @@
 #Here we store the objects used in the app
-import datetime
-from flask_mongoengine import MongoEngine
+
+from mongoengine import *
+import  datetime
+
+
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash,check_password_hash
+
+connect('todoflaskdb')
+
+
+class User(UserMixin,Document):
+    id = IntField(required=True)
+    email = EmailField(required=True,max_length=50, unique=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+    password = StringField(required=True,max_length=200)
+
+
+STATUS = (('D','Done'),
+          ('T','ToDo'))
+
+
+class ToDoTasks(Document):
+    id = IntField(required=True)
+    description = StringField(required=True,max_length=500)
+    dueDate = DateTimeField()
+    dateCreated = DateTimeField(default= datetime.datetime.now)
+    tags = ListField(StringField(max_length=30))
+    active = StringField(max_length=2, choices=STATUS)
+    timeWorked = DecimalField()
+    user = ReferenceField(User,reverse_delete_rule=CASCADE)
+    meta = {'allow_inheritance': True}
+
+class DoneTasks(Document):
+    id = IntField(required=True)
+    description = StringField(required=True,max_length=500)
+    dueDate = DateTimeField()
+    dateCreated = DateTimeField(default= datetime.datetime.now)
+    tags = ListField(StringField(max_length=30))
+    active = StringField(max_length=2, choices=STATUS)
+    timeWorked = DecimalField()
+    user = ReferenceField(User,reverse_delete_rule=CASCADE)
+    meta = {'allow_inheritance': True}
 
 
 
-db = MongoEngine('todoflaskdb')
 
-class User(UserMixin,db.Document):
-    id = db.IntField(required=True)
-    email = db.StringField(required=True,max_length=50, unique=True)
-    first_name = db.StringField(max_length=50)
-    last_name = db.StringField(max_length=50)
-    password = db.StringField(required=True,max_length=200)
-
-
-class ToDoTasks(db.Document):
-    id = db.IntField(required=True)
-    description = db.StringField(required=True,max_length=500, unique=True)
-    dueDate = db.DateTimeField()
-    dateCreated = db.DateTimeField(default= datetime.datetime.now)
-    tags = db.ListField(db.StringField(max_length=30))
-    active = db.RadioField()
-    timeWorked = db.DecimalField()
-
-class DoneTasks(db.Document):
-    id = db.IntField(required=True)
-    description = db.StringField(required=True,max_length=500, unique=True)
-    dueDate = db.DateTimeField()
-    dateCreated = db.DateTimeField(default= datetime.datetime.now)
-    tags = db.ListField(db.StringField(max_length=30))
-    active = db.RadioField()
-    timeWorked = db.DecimalField()
 
 
 
